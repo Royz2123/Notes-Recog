a1057d48941acc77c96fd5184335f31dbaf2b437
ykaner <ykaner12@gmail.com>
2017-10-01 00:32:57 +0300

    merge conflicts

--- note_train.py
+++ note_train.py
@@ -27,7 +27,6 @@ test_set = test_set.astype('float32')
 train_set /= 255
 test_set /= 255
 print(train_set.shape)
-<<<<<<< HEAD
 
 
 def train_NN(reload=False):
@@ -49,7 +48,7 @@ def train_NN(reload=False):
         acc = 0.0 if len(accuracies) == 0 else accuracies[-1]
         iteration = len(nets)
 
-    while acc < 0.95 and iteration < 100:
+    while acc < 0.965 and iteration < 100:
         print('start iteration %d' % (iteration + 1))
         train_res = network.fit(train_set, keras.utils.to_categorical(train_labels, num_classes=output_classes),
                     epochs=10, batch_size=50,
@@ -80,9 +79,6 @@ def train_NN(reload=False):
 
 if __name__ == '__main__':
     train_NN(reload=True)
-=======
-train_labels = train[1]
-test_labels = test[1]
 
 '''
 permutation = np.arange(len(train_set))
@@ -92,35 +88,3 @@ print(type(permutation))
 print(permutation)
 train_labels = train_labels[permutation]
 '''
-
-network = NN_util.create_net(input_size, output_classes)
-
-acc = 0.0
-iteration = 0
-accuracies = []
-nets = []
-while acc < 0.9 and iteration < 150:
-    print('start iteration %d' % iteration)
-    train_res = network.fit(train_set, keras.utils.to_categorical(train_labels, num_classes=output_classes),
-                epochs=1, batch_size=50,
-                validation_data=(test_set, keras.utils.to_categorical(test_labels, num_classes=output_classes)))
-    train_res = train_res.history
-
-#    eval_res = network.evaluate(test_set, keras.utils.to_categorical(test_labels, num_classes=output_classes),
-#                                batch_size=50)
-    acc = train_res['val_acc']
-    if type(acc) is list:
-        if len(acc) > 1:
-            print('len(acc) is more than 1, %d' % len(acc))
-        acc = acc[0]
-    accuracies.append(acc)
-    nets.append(NN_util.network_data(network))
-    iteration += 1
-    print('eval acc: ' + str(acc))
-
-with open('network_list', 'wb') as f:
-    pickle.dump(nets, f)
-with open('accuracies', 'wb') as f:
-    pickle.dump(accuracies, f)
-plot(accuracies)
->>>>>>> 005085ec30d0de5b418eafd53da4443a2be9df1c
